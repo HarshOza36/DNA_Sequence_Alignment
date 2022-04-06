9da94a300d3f47fe7d110586b5311f5524331dc7
HarshOza36 <harshoza3636@gmail.com>
2022-04-06 10:16:28 -0700

    Create class and remove files

--- basic_3.py
+++ basic_3.py
@@ -1,13 +1,51 @@
-from utils import parseInput
-from fixed_constants import acgt_indices, alpha_values, delta_e
 import sys
 
+class Utils:
+    def __init__(self):
+        self.alpha_values = [[0, 110, 48, 94],
+                            [110, 0, 118, 48],
+                            [48, 118, 0, 110],
+                            [94, 48, 110, 0]]
+        self.delta_e = 30
 
+    def generateString(self,xy_str,indices):
+        original_len = len(xy_str)
+        for i in indices:
+            xy_str = xy_str[:i+1] + xy_str + xy_str[i+1:]
+            
+        # Checking is the generated string matches the required length
+        assert len(xy_str) == ((2**len(indices)) * original_len), ">>> New String generated is not matching the expected length!"
+        return xy_str
+
+    def parseInput(self,filename):
+        with open(filename, 'r') as f:
+            # using rstrip to remove \n at the last
+            baseStrX = f.readline().rstrip()
+
+            X_indices = []
+            for line in f:
+                line = line.rstrip()
+                if line.isnumeric() is False: # Hence Indices are over the next base string started
+                    break
+                X_indices.append(int(line))
+
+            baseStrY = line.rstrip()
+            Y_indices = []
+            for line in f:
+                line = line.rstrip()
+                Y_indices.append(int(line))
+
+        if(baseStrX is None or baseStrY is None):
+            print(">>> The Base Strings are None.\nPlease try again!")
+        dnaStrX = self.generateString(baseStrX, X_indices)
+        dnaStrY = self.generateString(baseStrY, Y_indices)
+        return (dnaStrX.upper(), dnaStrY.upper())
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: python3 filename.py inputFile.txt")
         sys.exit()
-    dnaStrX, dnaStrY = parseInput(sys.argv[1])
+    obj = Utils()
+    dnaStrX, dnaStrY = obj.parseInput(sys.argv[1])
     print(dnaStrX, dnaStrY)
