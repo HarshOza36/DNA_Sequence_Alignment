74c6aad76a646bca074b1e32f4a5d0f16fedbb58
Shivam Kotak <shivamkotak@gmail.com>
2022-04-06 12:36:22 -0700

    Updated basic_3

--- basic_3.py
+++ basic_3.py
@@ -2,10 +2,11 @@ import sys
 
 class Utils:
     def __init__(self):
-        self.alpha_values = [[0, 110, 48, 94],
-                            [110, 0, 118, 48],
-                            [48, 118, 0, 110],
-                            [94, 48, 110, 0]]
+        self.alpha_values = {'A': {'A': 0, 'C': 110, 'G': 48, 'T': 94},
+        'C': {'A': 110, 'C': 0, 'G': 118, 'T': 48},
+        'G': {'A': 48, 'C': 118, 'G': 0, 'T': 110},
+        'T': {'A': 94, 'C': 48, 'G': 110, 'T': 0}
+        }
         self.delta_e = 30
 
     def generateString(self,xy_str,indices):
@@ -41,11 +42,80 @@ class Utils:
         dnaStrY = self.generateString(baseStrY, Y_indices)
         return (dnaStrX.upper(), dnaStrY.upper())
 
+    def write_output(self, op_file, data):
+        f = open(op_file, 'w')
+        for line in data:
+            f.write(line + '\n')
+
+        f.close()
+
+def basic_alignment(X,Y, alpha, delta):
+    len_x = len(X)+1
+    #print(len_x)
+    len_y = len(Y)+1
+    seq_1 = []
+    seq_2 = []
+    #dp_matrix = [[] for _ in range(len_y)]
+    dp_matrix = [[None for _ in range(len_y)] for _ in range(len_x)]
+    #print(dp_matrix)
+    for i in range(len_x):
+        dp_matrix[i][0] = i*delta
+
+    for i in range(1,len_y):
+        dp_matrix[0][i] = i*delta
+
+    #print(len(dp_matrix))
+    for j in range(1,len_y):
+        for i in range(1,len_x):
+            #print(i,j)
+            dp_matrix[i][j] = min(
+                dp_matrix[i-1][j-1] + alpha[X[i-1]][Y[j-1]],
+                dp_matrix[i][j-1] + delta,
+                dp_matrix[i-1][j] + delta
+            )
+    #print(dp_matrix[-1][-1])
+    i = len_x-1
+    j = len_y-1
+
+    while i>0 and j>0:
+        if dp_matrix[i-1][j-1] + alpha[X[i-1]][Y[j-1]] == dp_matrix[i][j]:
+            seq_1.append(X[i-1])
+            seq_2.append(Y[j-1])
+            i-=1
+            j-=1
+
+        elif dp_matrix[i-1][j] + delta == dp_matrix[i][j]:
+            seq_1.append(X[i-1])
+            seq_2.append('_')
+            i-=1
+
+        else:
+            seq_1.append('_')
+            seq_2.append(Y[j-1])
+            j-=1
+    
+    while i > 0:
+        seq_1.append(X[i-1])
+        seq_2.append('_')
+        i-=1
+
+    while j>0:
+        seq_1.append('_')
+        seq_2.append(Y[j-1])
+        j-=1
+
+    return [str(dp_matrix[-1][-1]), ''.join(seq_1[::-1]), ''.join(seq_2[::-1])]
+    #print(''.join(seq_1[::-1]))
+    #print('--------------------------------')
+    #print(''.join(seq_2[::-1]))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Usage: python3 filename.py inputFile.txt")
         sys.exit()
     obj = Utils()
     dnaStrX, dnaStrY = obj.parseInput(sys.argv[1])
-    print(dnaStrX, dnaStrY)
+    output_file = sys.argv[2]
+    #print(dnaStrX, dnaStrY)
+    sequences = basic_alignment(dnaStrX, dnaStrY, obj.alpha_values, obj.delta_e)
+    obj.write_output(output_file, sequences)
