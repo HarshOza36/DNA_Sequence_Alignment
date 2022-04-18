02ec5942f6d5f1c16cf272df7fbe9e2e6917fee1
HarshOza36 <harshoza3636@gmail.com>
2022-04-18 10:36:06 -0700

    final efficient dnc

--- efficient_3.py
+++ efficient_3.py
@@ -57,109 +57,71 @@ def process_memory():
   memory_consumed = int(memory_info.rss/1024)
   return memory_consumed
 
-
-def getSequences(X,Y,path,alpha,delta):
-    s1=""
-    s2=""
-    score=0
-    n=len(path) # n has to be equal to len(Y) 
-    i=0
-    j=0
-    while(i!=n and j!=n): # assuming i is increasing upwards and j is increasing towards the right
-        if (path[i]==path[i+1]):# next node is in the same column (upper row)
-            s1 = s1+X[i]
-            s2 = s2+'_'
-            score += delta
-            i += 1
-        elif (path[i]+1 == path[i+1]):# next node is one col to the right (upper row)
-            s1 = s1+X[i]
-            s2 = s2+Y[j]
-            score += alpha[X[i]][Y[j]]
-            i+=1
-            j+=1
-        else:  # next node is more than one col to the right, thus go right one col
-            s1 = s1+'_'
-            s2 = s2+Y[j]
-            score += delta
-            j += 1
-    return [str(score),s1,s2]
-    
-
-
-def basic_alignment(X, Y, alpha, delta):
+def align(X,Y,alpha, delta):
     len_x = len(X)+1
     len_y = len(Y)+1
 
-<<<<<<< HEAD
 
-=======
-    #dp_matrix = [[] for _ in range(len_y)]
->>>>>>> 74c0114115e83093188b7c402a4c82eae8a900ff
     dp_matrix = [[None for _ in range(len_y)] for _ in range(len_x)]
-    #print(dp_matrix)
-    for i in range(len_x):  # leftmost column
-        dp_matrix[i][0] = i*delta
+    for i in range(len_x):
+        dp_matrix[i][0] = i * delta
 
-<<<<<<< HEAD
     for i in range(1,len_y):
         dp_matrix[0][i] = i * delta
     
     
     for i in range(1,len_x):
         for j in range(1,len_y):
-=======
-    for i in range(1, len_y):  # bottom row
-        dp_matrix[0][i] = i*delta
-
-    # down to top left to right
-    for i in range(1, len_x):
-        for j in range(1, len_y):
->>>>>>> 74c0114115e83093188b7c402a4c82eae8a900ff
             dp_matrix[i][j] = min(
                 dp_matrix[i-1][j-1] + alpha[X[i-1]][Y[j-1]],
                 dp_matrix[i][j-1] + delta,
                 dp_matrix[i-1][j] + delta
             )
-<<<<<<< HEAD
 
         dp_matrix[i-1] = [] 
     return dp_matrix[-1] 
-=======
-        dp_matrix[i-1] = [] # removing rows to save space
 
-    return dp_matrix[-1]  # returning topmost row
->>>>>>> 74c0114115e83093188b7c402a4c82eae8a900ff
-
-
-def findMiddleNode(X, Y, n, alpha, delta):
-
-    # XL and all of Y
-    left_array = basic_alignment(
-        X[:n//2], Y, alpha, delta)
-
-    # XR and all of Y
-    right_array = basic_alignment(
-        X[n//2:][::-1], Y[::-1], alpha, delta)
-
-    # To find lowest among middle column of y
-    # left matrix has y values in last row(lenx-1) from 1 to leny-1
-    # right matrix has y values in first row(0) from 1 to leny-1 in reverse order
-
-    low = 1
-    m = len(left_array)
-    for j in range(1, m):
-        if(left_array[j]+right_array[m-1-j] < left_array[low]+right_array[m-1-low]):
-            low = j
-
-    # return index of j
-    return low
-
-
-def dnc_alignment(X, Y, ystart, alpha, delta):
-    n = len(X)
-    m = len(Y)
+def basic_alignment(X, Y, alpha, delta):
+    len_x = len(X) + 1
+    len_y = len(Y) + 1
+    seq_1 = []
+    seq_2 = []
     
-<<<<<<< HEAD
+    dp_matrix = [[None for _ in range(len_y)] for _ in range(len_x)]    
+    for i in range(len_x):
+        dp_matrix[i][0] = i * delta
+
+    for i in range(1,len_y):
+        dp_matrix[0][i] = i * delta
+    
+    for j in range(1,len_y):
+        for i in range(1,len_x):            
+            dp_matrix[i][j] = min(
+                dp_matrix[i-1][j-1] + alpha[X[i-1]][Y[j-1]],
+                dp_matrix[i][j-1] + delta,
+                dp_matrix[i-1][j] + delta
+            )
+
+    i = len_x-1
+    j = len_y-1
+
+    while i > 0 and j > 0:
+        if(dp_matrix[i-1][j-1] + alpha[X[i-1]][Y[j-1]] == dp_matrix[i][j]):
+            seq_1.append(X[i-1])
+            seq_2.append(Y[j-1])
+            i -= 1
+            j -= 1
+
+        elif(dp_matrix[i][j-1] + delta == dp_matrix[i][j]):
+            seq_1.append('_')
+            seq_2.append(Y[j-1])
+            j -= 1
+
+        else:
+            seq_1.append(X[i-1])
+            seq_2.append('_')
+            i -= 1
+    
     while i > 0:
         seq_1.append(X[i-1])
         seq_2.append('_')
@@ -171,9 +133,6 @@ def dnc_alignment(X, Y, ystart, alpha, delta):
         j -= 1
 
     return [dp_matrix[-1][-1], ''.join(seq_1[::-1]), ''.join(seq_2[::-1])]
-
-
-
 
 def dnc_alignment(X,Y, alpha, delta):
     data = dnc_alignment_helper(X,Y, alpha, delta)
@@ -196,27 +155,6 @@ def dnc_alignment_helper(X,Y, alpha, delta):
    left_half = dnc_alignment(X[:n//2], Y[:cut], alpha, delta)
    right_half = dnc_alignment(X[n//2:], Y[cut:], alpha, delta) 
    return [(left_half[i] + right_half[i]) for i in range(len(left_half))]
-=======
-    # [TO DO] BASE CONDITION: CAN'T SEEM TO UNDERSTAND WHAT THE BASE CONDITION COULD BE TO RETURN J
-#     https://www.youtube.com/watch?v=3TfDm8GpWRU&list=LL&index=1&t=126s     Video has some of the algorithm in it
-    if n < 2 or m < 2:
-        return [ystart+1]
-    # DIVIDE STEP
-    ycut = findMiddleNode(X, Y, n, alpha, delta)
-    ycut = ycut + ystart # added ystart to ycut so that the j remains consistent for its position in Y[]
-
-    # CONQUER RECURSIVE STEP gives the optimal solution (not value) (basically same as the top down pass)
-    left = dnc_alignment(X[:n//2], Y[:ycut], ystart, alpha, delta)
-    right = dnc_alignment(X[n//2:], Y[ycut:], ycut, alpha, delta)
-
-
-    # COMBINE STEP JUST CONCATENATE
-#     print(len(left+[ycut]+right))
-#     print(left+[ycut]+right) # Check if the final list is increasing from j = 0 to n-1
-
-    return left+[ycut]+right
-
->>>>>>> 74c0114115e83093188b7c402a4c82eae8a900ff
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -229,18 +167,7 @@ if __name__ == '__main__':
     # memory_info = process.memory_info()
     # tracemalloc.start()
     start_time = time.time()
-<<<<<<< HEAD
     memory_consumed, data = dnc_alignment(dnaStrX, dnaStrY, obj.alpha_values, obj.delta_e)
-=======
-#     data = dnc_alignment(dnaStrX, dnaStrY, obj.alpha_values, obj.delta_e)
-
-    aligned_path = dnc_alignment(
-        dnaStrX, dnaStrY, 0, obj.alpha_values, obj.delta_e)
-    
-    # iterate through all indices to know the score and the alignment
-    data = getSequences(dnaStrX, dnaStrY, aligned_path,
-                             obj.alpha_values, obj.delta_e)
->>>>>>> 74c0114115e83093188b7c402a4c82eae8a900ff
     end_time = time.time()
     time_taken = (end_time - start_time)*1000
     # memory_consumed = int(memory_info.rss/1024)
@@ -250,25 +177,3 @@ if __name__ == '__main__':
     print(f"{time_taken:.2f} ms")
     obj.write_output(output_file, data, time_taken, memory_consumed)
    
-
-
-
-# For testing comment out above main and run using this one. This is the input1 from the sample test cases.
-
-
-# if __name__ == '__main__':
-#     obj = Utils()
-#     dnaStrX, dnaStrY = "ACACACTGACTACTGACTGGTGACTACTGACTGGACTGACTACTGACTGGTGACTACTGACTGG", "TATTATTATACGCTATTATACGCGACGCGGACGCGTATACGCTATTATACGCGACGCGGACGCG"
-#     aligned_path = dnc_alignment(
-#         dnaStrX, dnaStrY, 0, obj.alpha_values, obj.delta_e)
-    
-#     # iterate through all indices to know the score and the alignment
-#     data = getSequences(dnaStrX, dnaStrY, aligned_path,
-#                              obj.alpha_values, obj.delta_e)
-#     print(data)
-    
-    
-    # required output: 
-    # 1296,
-    # _A_CA_CACT__G__A_C_TAC_TGACTG_GTGA__C_TACTGACTGGACTGACTACTGACTGGTGACTACT_GACTG_G
-    # TATTATTA_TACGCTATTATACGCGAC_GCG_GACGCGTA_T_AC__G_CT_ATTA_T_AC__GCGAC_GC_GGAC_GCG
